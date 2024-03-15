9801f61cf7b8682d783a0f5e17ad281c409d4d0a
Rahul Verma <105972307+RahulU19CN312@users.noreply.github.com>
2024-03-15 19:27:24 +0530

    Add files via upload

--- Enc_Dec.py
+++ Enc_Dec.py
@@ -3,18 +3,18 @@ class TranspositionCipher:
         self.key = key
 
     def encrypt(self, plaintext):
-        # Remove spaces and convert the plaintext to uppercase
+        
         plaintext = plaintext.replace(" ", "").upper()
 
-        # Calculate the number of rows required based on the key
+        
         num_rows = len(plaintext) // self.key
         if len(plaintext) % self.key != 0:
             num_rows += 1
 
-        # Create a 2D list to represent the grid
+        
         grid = [['' for _ in range(self.key)] for _ in range(num_rows)]
 
-        # Populate the grid with characters from the plaintext
+        
         index = 0
         for col in range(self.key):
             for row in range(num_rows):
@@ -22,12 +22,11 @@ class TranspositionCipher:
                     grid[row][col] = plaintext[index]
                     index += 1
 
-        # Combine characters from each column to get the encrypted message
+        
         encrypted_message = ''.join([''.join(row) for row in grid])
 
         return encrypted_message
 
-# Example Usage:
 try:
     cipher_key = int(input("Enter the key : "))
     transposition_cipher = TranspositionCipher(cipher_key)
